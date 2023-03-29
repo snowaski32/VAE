a337afdf84d975b233b41c3c79259335092ad3b1
Charles DuHadway <cduhadawy@iconbuild.com>
2023-03-29 17:17:10 +0000

    update dataset

--- clean_dataset.py
+++ clean_dataset.py
@@ -3,10 +3,12 @@ from torchvision.io import read_image
 import torch
 import torchvision
 
+dataset_name = 'with_dilation'
+
 count = 0
-for file in os.listdir("images"):
+for file in os.listdir(f"images/{dataset_name}/"):
     if 'png' in file:
-        path = f'images/{file}'
+        path = f'images/{dataset_name}/{file}'
         img = read_image(path, torchvision.io.ImageReadMode.GRAY)
 
         if torch.mean(img, dtype=float) < 5.0:
