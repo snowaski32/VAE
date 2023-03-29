c37f1253d91d19681e6ed6f12b3442ce50edfffe
Charles DuHadway <cduhadawy@iconbuild.com>
2023-03-29 16:44:09 +0000

    Faster Dataset

--- make_dataset.py
+++ make_dataset.py
@@ -5,77 +5,112 @@ import pytesseract as tes
 import numpy as np
 import torchvision.transforms as T
 import torchvision.utils as vutils
+from torch.utils.data import DataLoader, Dataset
 
 load_path = "/home/ec2-user/.floorplans"
 save_path = "images/"
+data_name = "with_dilation"
 
 patch_size = 512
 
-kernel = np.ones((3, 3), np.uint8)
-kernel2 = np.ones((5, 5), np.uint8)
 
-transform1 = T.Resize((patch_size, patch_size))
-transform2 = T.ToTensor()
+class RPLanDataset(Dataset):
+    def __init__(self, load_path, save_path, data_name, patch_size):
+        self.load_path = load_path
+        self.patch_size = patch_size
+        self.data_name = data_name
+        self.save_path = save_path
 
-file_names = []
+        self.toTensor = T.ToTensor()
 
-if os.path.exists(save_path + "/list.txt"):
-    with open(save_path + "/list.txt") as f:
-        lines = f.read().split('\n')
-        for line in lines:
-            if line != '':
-                file_names.append(line)
-else:
-    with open(load_path + "/good_examples.txt") as f:
-        lines = f.readlines()
-        for line in lines:
-            if len(line.split()) == 2:
-                file_name, conf = line.split()
-                print(file_name)
-                if (
-                    file_name.lower().endswith((".png", ".jpeg"))
-                ):
-                    img = read_image(load_path + "/plans/" + file_name)
-                    if (
-                        float(conf) >= 0.99
-                        and img.shape[1] <= 600
-                        and img.shape[2] <= 600
-                    ):
-                        file_names.append(file_name)
+        self.kernel = np.ones((3, 3), np.uint8)
+        self.kernel2 = np.ones((3, 3), np.uint8)
+        self.rect_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
 
-    with open(save_path + '/list.txt', 'w') as f:
-        f.writelines(name + '\n' for name in file_names)
+        file_names = []
+        if os.path.exists(save_path + "/list.txt"):
+            with open(save_path + "/list.txt") as f:
+                lines = f.read().split("\n")
+                for line in lines:
+                    if line != "":
+                        file_names.append(line)
+        else:
+            with open(load_path + "/good_examples.txt") as f:
+                lines = f.readlines()
+                for line in lines:
+                    if len(line.split()) == 2:
+                        file_name, conf = line.split()
+                        print(file_name)
+                        if file_name.lower().endswith((".png", ".jpeg")):
+                            img = read_image(load_path + "/plans/" + file_name)
+                            if (
+                                float(conf) >= 0.99
+                                and img.shape[1] <= 600
+                                and img.shape[2] <= 600
+                            ):
+                                file_names.append(file_name)
 
-names = []
-for i, f in enumerate(file_names):
-    print(i)
-    path = load_path + "/plans/" + f
-   
+            with open(save_path + "/list.txt", "w") as f:
+                f.writelines(name + "\n" for name in file_names)
 
-    image = cv.imread(path)
+        self.file_names = file_names
 
-    results = tes.image_to_boxes(image, output_type=tes.Output.DICT)
+    def __len__(self):
+        return len(self.file_names)
 
-    if 'char' in results:
-        for j in range(0, len(results['char'])):
-            if results['char'][j] != '~':
-                x = results['left'][j]
-                y = image.shape[0] - results['top'][j]
+    def __getitem__(self, idx):
+        path = self.load_path + "/plans/" + self.file_names[idx]
 
-                x2 = results['right'][j]
-                y2 = image.shape[0] - results['bottom'][j]
+        image = cv.imread(path)
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-                image = cv.rectangle(image, (x, y), (x2, y2), (int(image[0][0][0]), int(image[0][0][1]), int(image[0][0][2])), -1)
+        results = tes.image_to_boxes(image, output_type=tes.Output.DICT)
+        if "char" in results:
+            for j in range(0, len(results["char"])):
+                if results["char"][j] != "~":
+                    x = results["left"][j]
+                    y = image.shape[0] - results["top"][j]
 
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    image = cv.resize(image, (patch_size, patch_size))
-    # image = cv.dilate(image, kernel2)
-    image = cv.Canny(image, 100, 200)
-    # image = cv.dilate(image, kernel)
-    image = transform2(image)
+                    x2 = results["right"][j]
+                    y2 = image.shape[0] - results["bottom"][j]
 
-    vutils.save_image(image, f"{save_path}image{i}.png")
-    names.append(f"image{i}.png")
+                    image = cv.rectangle(
+                        image,
+                        (x, y),
+                        (x2, y2),
+                        (int(image[0][0][0]), int(image[0][0][1]), int(image[0][0][2])),
+                        -1,
+                    )
+        image = cv.resize(image, (patch_size, patch_size))
+        image = cv.dilate(image, self.rect_kernel)
+        image = cv.Canny(image, 100, 200)
+        image = cv.dilate(image, self.rect_kernel)
 
-with open(save_path + '/images.txt', 'w') as f:
-    f.writelines(name + '\n' for name in names)
+        # image = cv.erode(image, self.kernel)
+
+        image = self.toTensor(image)
+
+        vutils.save_image(image, f"{self.save_path}/{self.data_name}/image{idx}.png")
+
+        return path
+
+
+dataset = RPLanDataset(
+    load_path,
+    save_path,
+    data_name,
+    patch_size,
+)
+
+data_loader = DataLoader(
+    dataset,
+    batch_size=1,
+    num_workers=16,
+    shuffle=False,
+    pin_memory=False,
+)
+
+# next(iter(data_loader))
+# dataset[32]
+for path in data_loader:
+    pass
